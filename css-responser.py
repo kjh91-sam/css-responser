c10dd27e21e4b7fde74e051e8171272d132bec31
Sam Kang <sam.kang@3i.ai>
2021-09-24 15:54:48 +0900

    split functions

--- css-responser.py
+++ css-responser.py
@@ -15,27 +15,7 @@ DESKTOP_BRKP_RATIO = [round(p / DESKTOP, 2) for p in DESKTOP_BRKP]
 
 f.close()
 
-
-def responsive_styling(styling_units, isMobile=True, brkp_i=0):
-    responsive_styling = ""
-
-    for unit in styling_units:
-        responsive_styling += unit['selector'] + " {\n"
-
-        for style in unit['styles']:
-            pixel_vals = re.findall(r"(\d+)px", style)
-            for value in pixel_vals:
-                int_val = int(value)
-                style = style.replace(value + "px", str(round(int_val / 414 * 100, 2)) + "vw" if isMobile else
-                                      str(round(int_val * DESKTOP_BRKP_RATIO[brkp_i], 2)) + "px")
-            responsive_styling += style + "\n"
-
-        responsive_styling += "\t}"
-
-    return f"@media (max-width:{750 if isMobile else DESKTOP_BRKP[brkp_i]}px) {{ {responsive_styling} \n}}\n"
-
-
-def css_response(stylesheet):
+def stylesheet_parser(stylesheet):
     matches = re.finditer(
         r"(?P<selector>.*?){(?P<styles>.*?)}", stylesheet, re.MULTILINE | re.DOTALL)
     styling_units = [
@@ -46,15 +26,35 @@ def css_response(stylesheet):
         for m in matches
     ]
     styling_units = [unit for unit in styling_units if len(unit["styles"]) > 0]
+    return styling_units
+
+def calc_by_brkp(styling_units, isMobile=True, brkp_i=0):
+    calculated_styling = ""
+
+    for unit in styling_units:
+        calculated_styling += unit['selector'] + " {\n"
+
+        for style in unit['styles']:
+            pixel_vals = re.findall(r"(\d+)px", style)
+            for value in pixel_vals:
+                int_val = int(value)
+                style = style.replace(value + "px", str(round(int_val / 414 * 100, 2)) + "vw" if isMobile else
+                                      str(round(int_val * DESKTOP_BRKP_RATIO[brkp_i], 2)) + "px")
+            calculated_styling += style + "\n"
+
+        calculated_styling += "\t}"
+
+    return f"@media (max-width:{750 if isMobile else DESKTOP_BRKP[brkp_i]}px) {{ {calculated_styling} \n}}\n"
+
+def convert_stylesheet(stylesheet):
+    styling_units = stylesheet_parser(stylesheet)
 
     result = ""
-
     # desktop
     for i in range(len(DESKTOP_BRKP)):
-        result += responsive_styling(styling_units, False, i)
-
+        result += calc_by_brkp(styling_units, False, i)
     # mobile
-    result += responsive_styling(styling_units, True)
+    result += calc_by_brkp(styling_units, True)
 
     return result
 
@@ -67,11 +67,11 @@ for file_name in goal_file_names:
     f.close()
 
     if os.path.splitext(file_name) == '.css':
-        response = css_response(file_content)
+        response = convert_stylesheet(file_content)
     else:
         matches = re.findall(r"(?<=<style>)(.*?)(?=<\/style>)",
                              file_content, re.MULTILINE | re.DOTALL)
-        response = css_response("\n".join(matches))
+        response = convert_stylesheet("\n".join(matches))
 
     f = open("responsive_css_for_" + file_name + ".css", "w")
     f.write(response)
